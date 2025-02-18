84469e1ee2021249e001304c1b46a1f71dcecd50
raul <raul.cabello@suse.com>
2025-02-18 13:31:58 +0100

    Add OIDCClient type to imperative API

--- pkg/apis/ext.cattle.io/v1/types.go
+++ pkg/apis/ext.cattle.io/v1/types.go
@@ -1,24 +1,22 @@
 // +kubebuilder:skip
 package v1
 
-// This file should contain type definitions for the ext.cattle.io/v1 API group.
-// Below is an example of what it could look like. This comment can be removed once
-// types have been added to this package.
-//
-//	import (
-// 		metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-// 	)
-//
-// 	// +genclient
-// 	// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-//
-// 	type TestType struct {
-// 		metav1.TypeMeta   `json:",inline"`
-// 		metav1.ObjectMeta `json:"metadata,omitempty"`
-//
-// 		Spec TestTypeSpec `json:"ok"`
-// 	}
-//
-// 	type TestTypeSpec struct {
-// 		Enabled bool `json:"enabled"`
-// 	}
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// OIDCClient represents an application that uses Rancher OIDC provider as its authentication provider.
+type OIDCClient struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec OIDCClientSpec `json:"spec"`
+}
+
+type OIDCClientSpec struct {
+	Secret               string      `json:"secret"`
+	RedirectURIs         []string    `json:"redirectURIs"`
+	TokeLifeSpan         metav1.Time `json:"tokeLifeSpan"`
+	RefreshTokenLifeSpan metav1.Time `json:"refreshTokenLifeSpan"`
+}
